79c1e0f78f6d64ef148436f4121c168d288e1843
aevseev <contact@aevseev.ru>
2019-03-31 19:28:45 +0300

    Added leakCanary in the project

--- app/src/main/java/ru/teamdroid/recipecraft/AndroidApplication.kt
+++ app/src/main/java/ru/teamdroid/recipecraft/AndroidApplication.kt
@@ -2,6 +2,7 @@ package ru.teamdroid.recipecraft
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import com.squareup.leakcanary.LeakCanary
 import ru.teamdroid.recipecraft.data.DaggerRecipeRepositoryComponent
 import ru.teamdroid.recipecraft.data.RecipeRepositoryComponent
 
@@ -13,6 +14,7 @@ class AndroidApplication : Application() {
         super.onCreate()
         initializeDependencies()
         Stetho.initializeWithDefaults(this)
+        LeakCanary.install(this)
     }
 
     private fun initializeDependencies() {
