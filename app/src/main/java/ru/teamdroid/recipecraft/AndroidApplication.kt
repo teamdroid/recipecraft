b00ada7aab559bca0e3c2829c8ad6f5708d8f813
aevseev <contact@aevseev.ru>
2019-04-20 22:59:22 +0300

    Retrieved stetho library

--- app/src/main/java/ru/teamdroid/recipecraft/AndroidApplication.kt
+++ app/src/main/java/ru/teamdroid/recipecraft/AndroidApplication.kt
@@ -1,6 +1,7 @@
 package ru.teamdroid.recipecraft
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.squareup.leakcanary.LeakCanary
 import ru.teamdroid.recipecraft.data.components.DaggerRecipeRepositoryComponent
 import ru.teamdroid.recipecraft.data.components.RecipeRepositoryComponent
@@ -12,6 +13,7 @@ class AndroidApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initializeDependencies()
+        Stetho.initializeWithDefaults(this)
         LeakCanary.install(this)
     }
 
