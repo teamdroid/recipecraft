2a7634e7e810c661ac6f3afd2afa28ae50dba556
aevseev <contact@aevseev.ru>
2019-09-21 15:12:53 +0300

    Added flipper for analyze

--- app/src/main/java/ru/teamdroid/recipecraft/AndroidApplication.kt
+++ app/src/main/java/ru/teamdroid/recipecraft/AndroidApplication.kt
@@ -4,6 +4,14 @@ import android.app.Application
 import com.squareup.leakcanary.LeakCanary
 import ru.teamdroid.recipecraft.data.components.DaggerRecipeRepositoryComponent
 import ru.teamdroid.recipecraft.data.components.RecipeRepositoryComponent
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.soloader.SoLoader
+import com.facebook.flipper.plugins.leakcanary.LeakCanaryFlipperPlugin
+import com.facebook.flipper.plugins.leakcanary.RecordLeakService
 
 class AndroidApplication : Application() {
 
@@ -12,7 +20,19 @@ class AndroidApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initializeDependencies()
-       // LeakCanary.install(this)
+        SoLoader.init(this, false)
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(DatabasesFlipperPlugin(this))
+            client.addPlugin(LeakCanaryFlipperPlugin())
+
+            LeakCanary.refWatcher(this)
+                    .listenerServiceClass(RecordLeakService::class.java)
+                    .buildAndInstall()
+
+            client.start()
+        }
     }
 
     private fun initializeDependencies() {
